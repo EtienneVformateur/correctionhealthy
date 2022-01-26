087da75a0ee3ace9ef01992f233007921d5597f4
EtienneV <formation.etienne.re@gmail.com>
2022-01-26 14:13:16 +0400

    genreSelectionne et retirer fonction

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -5,7 +5,7 @@ import 'component/selectGenre.dart';
 
 const activeColor = Color(0xFF1D1E33);
 const inactiveCouleur = Color(0xFF111328);
-enum Genre { Homme, Femme }
+enum Genre { Nondefini, Homme, Femme }
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -15,43 +15,7 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  Color carteHommeCouleur = inactiveCouleur;
-  Color carteFemmeCouleur = inactiveCouleur;
-
-  updateCouleur(Genre genre) {
-    // CAS 1 : HOMME ACTIF ET FEMME DESACTIVE
-    // CAS 2 : FEMME ACTIVE et Homme DESACTIVE
-    // CAS 3 : HOMME et FEMME DESACTIVE
-
-    // CAS 3.1 : CLIQUE SUR HOMME
-    // CAS 3.2 : CLIQUE SUR FEMME
-
-    // CAS 1.1 : CLIQUE SUR HOMME
-    // CAS 1.2 : CLIQUE SUR FEMME
-
-    // CAS 2.1 : CLIQUE SUR HOMME
-    // CAS 2.2 : CLIQUE SUR FEMME
-
-    genre == Genre.Homme && carteHommeCouleur == inactiveCouleur
-        ? carteHommeCouleur = activeColor
-        : carteHommeCouleur = inactiveCouleur;
-    genre == Genre.Femme && carteFemmeCouleur == inactiveCouleur
-        ? carteFemmeCouleur = activeColor
-        : carteFemmeCouleur = inactiveCouleur;
-    // if (genre == Genre.Homme && carteHommeCouleur == inactiveCouleur) {
-    //   carteFemmeCouleur = inactiveCouleur;
-    //   carteHommeCouleur = activeColor;
-    // }
-    // else if (genre == Genre.Femme && carteFemmeCouleur == inactiveCouleur) {
-    //   carteFemmeCouleur = activeColor;
-    //   carteHommeCouleur = inactiveCouleur;
-    // } else if (genre == Genre.Homme) {
-    //   carteHommeCouleur = inactiveCouleur;
-    // } else if (genre == Genre.Femme) {
-    //   carteFemmeCouleur = inactiveCouleur;
-    // }
-    ;
-  }
+  Genre genreSelectionne = Genre.Nondefini;
 
   @override
   Widget build(BuildContext context) {
@@ -67,11 +31,13 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: MaCarte(
-                    carteHommeCouleur,
+                    genreSelectionne == Genre.Homme
+                        ? activeColor
+                        : inactiveCouleur,
                     GestureDetector(
                         onTap: () {
                           setState(() {
-                            updateCouleur(Genre.Homme);
+                            genreSelectionne = Genre.Homme;
                           });
                         },
                         child: SelectGenre("Homme", FontAwesomeIcons.mars)),
@@ -79,11 +45,13 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: MaCarte(
-                      carteFemmeCouleur,
+                      genreSelectionne == Genre.Femme
+                          ? activeColor
+                          : inactiveCouleur,
                       GestureDetector(
                           onTap: () {
                             setState(() {
-                              updateCouleur(Genre.Femme);
+                              genreSelectionne = Genre.Femme;
                             });
                           },
                           child: SelectGenre("Femme", FontAwesomeIcons.venus))),
